5975ecc113872237867e8348da2aa8e388ba06e9
Kai Dev Hu <kdhu@thoughtworks.com>
2015-12-04 18:46:37 +0800

    init project

---
+++ src/main/java/com/thoughtworks/pos/HelloGuys.java
@@ -0,0 +1,7 @@
+package com.thoughtworks.pos;
+
+public class HelloGuys {
+    public static void main(String[] args) {
+        System.out.println("Hello Guys!");
+    }
+}
---
+++ src/test/java/com/thoughtworks/pos/HelloGuysTest.java
@@ -0,0 +1,5 @@
+package com.thoughtworks.pos;
+
+public class HelloGuysTest {
+
+}
