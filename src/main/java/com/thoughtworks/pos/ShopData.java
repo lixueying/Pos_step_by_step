d1fb85d8b92d04df0c7e26d3146a9abb1d8dd809
Kai Dev Hu <kdhu@thoughtworks.com>
2015-12-05 12:48:48 +0800

    remove unuse code

--- src/main/java/com/thoughtworks/pos/ShopData.java
+++ src/main/java/com/thoughtworks/pos/ShopData.java
@@ -1,8 +1,7 @@
 package com.thoughtworks.pos;
 
-import com.thoughtworks.pos.strategy.*;
-
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
 
 public final class ShopData {
 
@@ -11,20 +10,4 @@ public final class ShopData {
 
     public static final List<String> SHOPPING_CART_DATA =
             Arrays.asList("ITEM000001-2", "ITEM000003-5", "ITEM000005-3");
-
-    public static final Map<String, PromotionStrategy> ALL_PROMOTIONS = new HashMap<>();
-
-    static {
-        PromotionStrategy item1Promotion =
-                new CompositeStrategy(Arrays.asList(new DiscountStrategy(75),new SecondHalfStrategy()));
-        ALL_PROMOTIONS.put("ITEM000001",item1Promotion);
-
-        PromotionStrategy item3Promotion =
-                new CompositeStrategy(Arrays.asList(new SecondHalfStrategy(),new ReduceXUponYStrategy(100, 10)));
-        ALL_PROMOTIONS.put("ITEM000003", item3Promotion);
-
-        PromotionStrategy item5Promotion =
-                new CompositeStrategy(Arrays.asList(new DiscountStrategy(90), new ReduceXUponYStrategy(300, 50)));
-        ALL_PROMOTIONS.put("ITEM000005", item5Promotion);
-    }
 }
